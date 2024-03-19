1717c013595e3974bf760357cdc2b27f40960988
skh9301 <157680899+skh9301@users.noreply.github.com>
2024-03-19 13:02:15 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner se = new Scanner(System.in);
+		int A= se.nextInt();
+		double B= se.nextInt();
+		double result = A/B;
+		System.out.println(result);
+		
+	}
+
+}
