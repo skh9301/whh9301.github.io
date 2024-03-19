c93ed02fcffae83f990c29b02164d2fc110fdc0e
skh9301 <157680899+skh9301@users.noreply.github.com>
2024-03-19 13:07:07 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner se = new Scanner(System.in);
+		String name= se.nextLine();
+		System.out.println(name+"??!");
+		
+	}
+
+}
